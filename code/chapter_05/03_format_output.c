5f74e20dc08d1fd363774a5abb9d2b5049f6546a
ZL85 <zongliang0726@163.com>
2024-01-31 22:54:37 +0800

    update

--- code/chapter_05/03_format_output.c
+++ code/chapter_05/03_format_output.c
@@ -1,3 +1,35 @@
 #include <stdio.h>
 
-int main() { return 0; }
+int main() {
+    signed int i_1 = 2;
+    printf("%d\n", i_1);
+    printf("%i\n", i_1);
+
+    unsigned int i_2 = 4;
+    printf("%u\n", i_2);
+
+    unsigned int i_3 = 0123;
+    printf("%o\n", i_3);
+    printf("%d\n", i_3);
+
+    unsigned int i_4 = 0x123;
+    printf("%x\n", i_4);
+    printf("%X\n", i_4);
+    printf("%d\n", i_4);
+
+    char c_1 = 'a';
+    printf("%c\n", c_1);
+    printf("%d\n", c_1);
+
+    char c_arr[10] = "hello c!";
+    printf("%s\n", c_arr);
+
+    float f_1 = 0.0054f;
+    printf("%f\n", f_1);
+    printf("%e\n", f_1);
+    printf("%g\n", f_1);
+    printf("*%10.3f\n", f_1);
+    printf("%-10.3f*\n", f_1);
+
+    return 0;
+}
--- code/chapter_05/04_format_input.c
+++ code/chapter_05/04_format_input.c
@@ -1,3 +1,49 @@
 #include <stdio.h>
 
-int main() { return 0; }
+int main() {
+    signed int i_1;
+    printf("please input signed int i_1:\n");
+    scanf_s("%d", &i_1);
+    printf("%d\n", i_1);
+    printf("%i\n", i_1);
+
+    unsigned int i_2;
+    printf("please input unsigned int i_2:\n");
+    scanf_s("%u", &i_2);
+    printf("%u\n", i_2);
+
+    unsigned int i_3;
+    printf("please input unsigned int i_3:\n");
+    scanf_s("%o", &i_3);
+    printf("%o\n", i_3);
+    printf("%d\n", i_3);
+
+    unsigned int i_4;
+    printf("please input unsigned int i_4:\n");
+    scanf_s("%x", &i_4);
+    printf("%x\n", i_4);
+    printf("%X\n", i_4);
+    printf("%d\n", i_4);
+
+    char c_1;
+    printf("please input char c_1:\n");
+    scanf_s("%c", &c_1);
+    printf("%c\n", c_1);
+    printf("%d\n", c_1);
+
+    char c_arr[10];
+    printf("please input char c_arr[10]:\n");
+    scanf_s("%s", c_arr);
+    printf("%s\n", c_arr);
+
+    float f_1;
+    printf("please input float f_1:\n");
+    scanf_s("%f", &f_1);
+    printf("%f\n", f_1);
+    printf("%e\n", f_1);
+    printf("%g\n", f_1);
+    printf("*%10.3f\n", f_1);
+    printf("%-10.3f*\n", f_1);
+
+    return 0;
+}
